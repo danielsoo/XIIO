decaa4c318c7d53f47c6f7dd7e8945a29abe5170
Younsoo Park <danielpark605@gmail.com>
2026-06-03 13:20:21 +0900

    Sidebar Icon_5

--- src/lib/homeHeroBackground.ts
+++ src/lib/homeHeroBackground.ts
@@ -1,2 +1,4 @@
 export const HERO_LANDSCAPE_IMAGE = "/images/hero-landscape.webp";
-export const HERO_LANDSCAPE_POSITION = "right center";
+export const HERO_LANDSCAPE_POSITION = "left top";
+export const HERO_LANDSCAPE_POSITION_COMPACT = "right center";
+export const HERO_LANDSCAPE_ASPECT = 630 / 300;
