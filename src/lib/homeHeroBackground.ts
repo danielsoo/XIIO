122ba54b3d956ac660804b997f99899718ec0968
Younsoo Park <danielpark605@gmail.com>
2026-06-03 18:34:00 +0900

    Home Background Image Changed

--- src/lib/homeHeroBackground.ts
+++ src/lib/homeHeroBackground.ts
@@ -1,2 +1,2 @@
-export const HERO_LANDSCAPE_IMAGE = "/images/hero-landscape.webp";
+export const HERO_LANDSCAPE_IMAGE = "/images/home-wave1.png";
 export const HERO_LANDSCAPE_POSITION = "right center";
