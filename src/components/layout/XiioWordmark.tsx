2c453aa7b97bf0d0469153a602ccbe192eea650e
Younsoo Park <danielpark605@gmail.com>
2026-06-08 19:37:51 +0900

    Logo_7

--- src/components/layout/XiioWordmark.tsx
+++ src/components/layout/XiioWordmark.tsx
@@ -12,11 +12,11 @@ export default function XiioWordmark({ className = "" }: Props) {
         <span className="absolute h-[0.72em] w-[0.1em] rounded-full bg-current rotate-45" />
         <span className="absolute h-[0.72em] w-[0.1em] rounded-full bg-current -rotate-45" />
       </span>
-      <span className="inline-flex h-[1em] items-center gap-[0.21em] shrink-0">
+      <span className="inline-flex h-[0.72em] items-center gap-[0.21em] shrink-0">
         <span className="w-[0.1em] h-full bg-current rounded-full" />
         <span className="w-[0.1em] h-full bg-current rounded-full" />
       </span>
-      <span className="ml-[0.32em] shrink-0 size-[1em] rounded-full border-[0.1em] border-current box-border" />
+      <span className="ml-[0.32em] shrink-0 size-[0.72em] rounded-full border-[0.1em] border-current box-border" />
     </span>
   );
 }
